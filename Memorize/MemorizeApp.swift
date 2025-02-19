168cc2222598a76f80285d803a2e3090819af79d
Jack <jack@dev.it>
2025-02-19 13:54:17 +0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Weerachai Anotaipaiboon on 19/2/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
